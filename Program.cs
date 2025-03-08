b82deaf6ae12fdc51356daf5dc68103f83b9d293
gurulinbo <as30995835@gmail.com>
2025-03-08 12:04:20 +0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+using System;
 using shape;
 
 //Shape myshape = new Shape();
 
-Shape mycircle1 = new Circle(5.4);
+Shape mycircle1 = new Circle(6.2);
 Shape mycircle2 = new Circle(2.5);
-Shape myrectangle = new Rectangle(2,5);
+Shape myrectangle = new Rectangle(9,8);
 Shape myrectangle2 = new Rectangle(2,10);
-Shape myTriangle = new Triangle(2,5);
+Shape myTriangle = new Triangle(9,5);
 Shape myTriangle2 = new Triangle(2,5,9);
+Shape myTrapezoid1 = new Trapezoid(10,5,4);
 
 
 
-Console.WriteLine("mycircile1:" + mycircle1.area.ToString() + " mycircile2:" + mycircle2.area.ToString());
-Console.WriteLine(msg);
+Console.WriteLine("mycircile1:" + mycircle1.area.ToString());
+Console.WriteLine(" mycircile2:" + mycircle2.area.ToString());
+string msg = "";
 Console.WriteLine("myrectangle:" + myrectangle.area.ToString());
 Console.WriteLine("myTriangle:" + myTriangle.area.ToString());
 Console.WriteLine("myrectangle:" + myrectangle2.perimeter.ToString());
 Console.WriteLine("myTriangle2:"  + myTriangle2.perimeter.ToString());
-string msg = "";
+Console.WriteLine("myTrapezoid1:" + myTrapezoid1.area.ToString());
 
-if (mycircle1.CompareTo(mycircle2) == 0)
-    msg = "一樣";
+if (mycircle1.area.CompareTo(myTriangle.area) > 0)
+{
+    if (myTrapezoid1.area.CompareTo(mycircle1.area) > 0)
+        Console.WriteLine("梯形 > 圓形 > 三角形");
+    else if (myTrapezoid1.area.CompareTo(mycircle1.area) == 0)
+        Console.WriteLine("梯形 = 圓形 > 三角形");
+    else if (myTriangle.area.CompareTo(myTrapezoid1.area) > 0)
+        Console.WriteLine("圓形 > 梯形 > 三角形");
+    else Console.WriteLine("圓形 > 三角形 > 梯形");
+}
+else if (mycircle1.area.CompareTo(myTriangle.area) < 0)
 
-if (mycircle1.CompareTo(mycircle2) > 0)
-    msg = "1號大";
-
-if (mycircle1.CompareTo(mycircle2) < 0)
-    msg = "2號大";
-
+    if (myTrapezoid1.area.CompareTo(myTriangle.area) > 0)
+        Console.WriteLine("梯形 > 三角形 > 圓形");
+    else if (myTrapezoid1.area.CompareTo(mycircle1.area) == 0)
+        Console.WriteLine("梯形 = 三角形 > 圓形");
+    else if (myTriangle.area.CompareTo(myTrapezoid1.area) > 0)
+        Console.WriteLine("三角形 > 梯形 > 圓形");
+    else Console.WriteLine("三角形 > 圓形 > 梯形");
+else
+{
+    if (myTrapezoid1.area.CompareTo(mycircle1.area) > 0)
+        Console.WriteLine("梯形 > 圓形 = 三角形");
+    else if (myTrapezoid1.area.CompareTo(mycircle1.area) < 0)
+        Console.WriteLine("圓形 = 三角形 > 梯形");
+    else Console.WriteLine("圓形 = 三角形 = 梯形");
+    
+}
+Console.WriteLine(msg);
